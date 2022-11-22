affce0fdfd754daf8eb6818e66f4d18f1de12399
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-11-22 18:44:08 -0500

    functions

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import time
 import pandas as pd
 
 
-
-groupNameList = ['A','B','C','D','E','F','G','H','I']
-listCount = 0
-groupName = 'A'
-url = f"https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_Group_{groupName}"
-
-
-
-while groupName != 'I':
+def updateCSV():
+    groupNameList = ['A','B','C','D','E','F','G','H','I']
+    listCount = 0
+    groupName = 'A'
     url = f"https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_Group_{groupName}"
-    r = requests.get(url)
-    df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
-    df = df_list[0] # returns the first 5 rows of the dataframe
-    df.head()
-    print(df)
-    df.to_csv(f'Group{groupName}.csv')
-    listCount = listCount + 1
-    groupName = groupNameList[listCount]
+
+
+
+    while groupName != 'I':
+        url = f"https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_Group_{groupName}"
+        r = requests.get(url)
+        df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
+        df = df_list[0] # returns the first 5 rows of the dataframe
+        df.head()
+        print(df)
+        df.to_csv(f'Group{groupName}.csv')
+        listCount = listCount + 1
+        groupName = groupNameList[listCount]
 
 
