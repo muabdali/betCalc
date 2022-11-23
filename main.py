ab46c731c33ef5a0e22e82ad9632b02938178408
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-11-22 19:41:46 -0500

    program now only returns top 2 teams

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import requests
 import bs4
 import time
 import pandas as pd
-import html5lib
-
+import csv
 
 def updateCSV():
+    global groupName 
+    global groupNameList 
+    global listCount
     groupNameList = ['A','B','C','D','E','F','G','H','I']
     listCount = 0
     groupName = 'A'
@@ -20,6 +22,8 @@ def updateCSV():
         df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
         df = df_list[1] # returns the first 5 rows of the dataframe
         df.head()
+        df.drop(index=3,inplace=True)
+        df.drop(index=2,inplace=True)
         print(df)
         df.to_csv(f'Group{groupName}.csv')
         listCount = listCount + 1
@@ -27,4 +31,35 @@ def updateCSV():
 
 
 
+"""
+def deleteUseless():
+    csv_file = f'Group{groupName}.csv'
+    file = open(csv_file)
+    csvreader = csv.reader(file)
+
+
+    next(csvreader)
+
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+    
+    file.close
+
+    with open(csv_file, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+"""
+
+"""
+def dbDeleteUseless():
+    df = pd.read_csv(f'Group{groupName}.csv')
+    df.drop(index=3,inplace=True)
+    df.drop(index=2,inplace=True)
+    print(df)
+
+"""
 updateCSV()
+# reset variables
+groupName = groupNameList[0]
+listCount = 0
