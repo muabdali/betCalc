5926e136e5b3e91d1593d544f8ed9847c5ec38b8
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-11-23 23:22:22 -0500

    new function that combines all group top 2's to make point calculating easier
    working on function to move groupStage files to a
    seperate folder

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import bs4
 import time
 import pandas as pd
 import csv
+import os
+import shutil
 
 def updateCSV():
     global groupName 
@@ -13,8 +15,6 @@ def updateCSV():
     listCount = 0
     groupName = 'A'
     url = f"https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_Group_{groupName}"
-
-
 
     while groupName != 'I':
         url = f"https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_Group_{groupName}"
@@ -29,6 +29,15 @@ def updateCSV():
         listCount = listCount + 1
         groupName = groupNameList[listCount]
 
+
+def moveFile():
+    groupManeList = ['A','B','C','D','E','F','G','H','I']
+    groupMane = 'A'
+    listCountt = 0
+    while groupMane != 'I':
+        os.rename(f"D:\betCalc-2\Group{groupMane}", f"D:\betCalc-2\Groups\Group{groupMane}")
+        listCountt = listCountt + 1
+        groupMane = groupManeList[listCount]
 
 
 """
@@ -69,6 +78,47 @@ def dbDeleteUseless():
 """
 updateCSV()
 
+
 # reset variables
 groupName = 'A'
 listCount = 0
+
+import csv
+import glob
+
+#creates combined standings
+def combinedStandings():
+    csv_files = {}      # (header as tuple) : csv.writer()
+    header_type_count = 1
+    for filename in glob.glob('*.csv'):
+        with open(filename, newline='') as f_input:
+            csv_input = csv.reader(f_input)
+            header = tuple(next(csv_input))
+            
+            try:
+                csv_files[header].writerows(csv_input)
+            except KeyError:
+                f_output = open(f'combinedStandings{header_type_count:02}.csv', 'w', newline='')
+                header_type_count += 1
+                csv_output = csv.writer(f_output)
+                csv_files[header] = csv_output
+                csv_output.writerow(header)
+                csv_output.writerows(csv_input)
+
+
+
+
+def findPointAny():
+    pointTotal = 0
+    teamName = "Netherlands"
+    with open('combinedStandings01.csv', 'rt') as f:
+     reader = csv.reader(f, delimiter=',')
+     for row in reader:
+          if teamName == row[2]: # if the username shall be on column 3 (-> index 2)
+              print ("is in file")
+
+
+#combinedStandings()
+findPointAny()
+moveFile()
+
